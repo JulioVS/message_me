97cbde6aba278ae33d3a3c7482628fdaf6554824
Julio Errecart <julioer@me.com>
2023-11-23 15:26:50 -0300

    Add login functionality

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,31 @@
 class ApplicationController < ActionController::Base
+  helper_method :current_user, :logged_in?
+
+  def current_user
+    # El significado de ||= en Ruby/Rails es:
+    #
+    # Si la variable de la izquierda no esta definida (es decir aun no existe en memoria)
+    # o tiene un valor nulo, entonces evaluo la expresion de la derecha y le asigno
+    # su resultado a la variable.-
+    #
+    # Si la variable de la izquierda ya existe y no tiene valor nulo, entones no hago nada
+    # (o sea la variable conserva su valor anterior a esta sentencia y sigue de largo)
+    #
+    @current_user ||= User.find(session[:user_id]) if session[:user_id] 
+  end
+
+  def logged_in?
+    # La doble negacion hace que este metodo siempre devuelva un valor booleano, 
+    # haya o no un usuario cargado en @current_user.
+    # En el caso de que efectivamente haya usuario logueado, queremos que devuelva "true"
+    # y NO el usuario logueado en si.-
+    !!current_user
+  end
+
+  def require_user
+    if !logged_in?
+      flash[:error] = "You must be logged in to perform that action"
+      redirect_to login_path
+    end
+  end
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,4 +2,28 @@ class SessionsController < ApplicationController
   def new
 
   end
+
+  def create
+    input_user = params[:session][:username]
+    input_pass = params[:session][:password] 
+
+    user = User.find_by(username: input_user)
+
+    if user && user.authenticate(input_pass)
+      session[:user_id] = user.id 
+      flash[:success] = "You have succesfully logged in"
+      redirect_to root_path 
+    else
+      flash.now[:error] = "There was something wrong with your login information"
+      render 'new' 
+    end
+
+    # debugger
+  end
+
+  def destroy
+    session[:user_id] = nil
+    flash[:success] = "You have successfully logged out"
+    redirect_to login_path
+  end
 end
