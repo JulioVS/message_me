3b0df9644f9bdffcc0d369715f37440d503362d1
Julio Errecart <julioer@me.com>
2023-11-24 12:57:27 -0300

    Fix previous commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  get 'messages/create'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
